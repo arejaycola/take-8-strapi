51f330cbbc2a062dfed742562b0d6d2279d90be4
Rob Mastronicola <rob.mastronicola@circulohealth.com>
2024-05-15 22:23:36 -0400

    qwef

--- config/middlewares.js
+++ config/middlewares.js
@@ -24,6 +24,7 @@ module.exports = [
         "https://localhost:3000",
         "http://wrestlingtournaments.com",
         'https://take8.vercel.app',
+        
       ],
       methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"],
       headers: [
