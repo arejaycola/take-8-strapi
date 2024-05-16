e9be56c8326ca98c6e25da84d9e2799788fefe7f
Rob Mastronicola <rob.mastronicola@circulohealth.com>
2024-05-15 22:44:49 -0400

    asdf

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => {
-    const client = env('DATABASE_CLIENT', 'sqlite');
+    const client = env('DATABASE_CLIENT', 'postgres');
     const connections = {
         postgres: {
             connection: {
